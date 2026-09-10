e7dcab54e05d7cbb1daf245f8047766bc7a027eb
GUILHERME WEBBER SCHEUER <guilherme_w_scheuer@estudante.sesisenai.org.br>
2026-09-10 12:30:31 -0300

    Mais coisa pra atividade

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,19 @@ server.get('/produtos/:id', (req, res) => {
     })
 })
 
+server.get('/produtos/busca/:nome', (req, res) => {
+    const sql = 'SELECT * FROM PRODUTO WHERE nome LIKE ?';
+
+    const termoBusca = '%' + req.params.nome + '%';
+
+    connection.query(sql, [termoBusca], (erro, resultado) => {
+        if(erro){
+            return res.status(500).json({erro: erro.message });
+        }
+        return res.json(resultado);
+    })
+})
+
 const PORT = 3099;
 
 server.listen(PORT, () => {
